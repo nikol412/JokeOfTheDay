5a80e726305a2fb9da43e68b3fda79c05c60bafd
nikol412 <travin.nikita@gmail.com>
2021-03-30 13:28:24 +0600

    added menu, navigation files, added drawerLayout

--- app/src/main/java/com/nikol412/jokeoftheday/MainActivity.kt
+++ app/src/main/java/com/nikol412/jokeoftheday/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.nikol412.jokeoftheday
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.nikol412.jokeoftheday.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfig = AppBarConfiguration(navController.graph, binding.drawerLayout)
+        with(binding) {
+            navView.setupWithNavController(navController)
+            toolbar.setupWithNavController(navController, appBarConfig)
+        }
+//        binding.navView.setupWithNavController(navController)
     }
 }
