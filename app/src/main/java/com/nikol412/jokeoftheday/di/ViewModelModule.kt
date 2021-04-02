0d45425aab1829d6d1e3fe55a66823c0c2c55412
nikol412 <travin.nikita@gmail.com>
2021-04-02 14:13:53 +0600

    added joke repository to favourite vm

--- app/src/main/java/com/nikol412/jokeoftheday/di/ViewModelModule.kt
+++ app/src/main/java/com/nikol412/jokeoftheday/di/ViewModelModule.kt
@@ -7,5 +7,5 @@ import org.koin.dsl.module
 
 val viewModelModule = module {
     viewModel { GetJokeVM(get(), get()) }
-    viewModel { FavouritesJokesViewModel() }
+    viewModel { FavouritesJokesViewModel(get()) }
 }
--- app/src/main/java/com/nikol412/jokeoftheday/ui/favourites/FavouritesJokesViewModel.kt
+++ app/src/main/java/com/nikol412/jokeoftheday/ui/favourites/FavouritesJokesViewModel.kt
@@ -2,20 +2,24 @@ package com.nikol412.jokeoftheday.ui.favourites
 
 import androidx.lifecycle.ViewModel
 import com.nikol412.jokeoftheday.api.JokeResponse
+import com.nikol412.jokeoftheday.db.model.JokeOfficial
+import com.nikol412.jokeoftheday.db.repository.JokesOfficialRepository
 import com.nikol412.jokeoftheday.ui.base.BaseViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.emitAll
 
-class FavouritesJokesViewModel : BaseViewModel() {
-    private val mutableFavouritesJokesList = MutableStateFlow(listOf<JokeResponse>())
-    val favouritesJokesList: StateFlow<List<JokeResponse>> = mutableFavouritesJokesList
+class FavouritesJokesViewModel(private val jokesOfficialRepository: JokesOfficialRepository) : BaseViewModel() {
+    private val mutableFavouritesJokesList = MutableStateFlow(listOf<JokeOfficial>())
+    val favouritesJokesList: StateFlow<List<JokeOfficial>> = mutableFavouritesJokesList
 
     init {
 
     }
 
     fun fetchJokes() {
-        val temp = (1 until 11).map { JokeResponse(it, "$it".repeat(it), "$it".repeat(it), "$it") }
-        mutableFavouritesJokesList.value = temp
+        jokesOfficialRepository.findStarred().addChangeListener { data ->
+            mutableFavouritesJokesList.value = data.toList()
+        }
     }
 }
